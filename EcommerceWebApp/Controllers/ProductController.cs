10db9b03a51a361d903eab99f731bc122a832337
DitebogoT <diditlhakola@gmail.com>
2025-10-20 13:31:56 +0200

    Add project files.

---
+++ EcommerceWebApp/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
