b453cf74bd4f674df863633edde45c363eb1a27a
DitebogoT <diditlhakola@gmail.com>
2025-10-20 16:53:15 +0200

    DapperContext code was added

---
+++ EcommerceWebApp.Data/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceWebApp.Data
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+        public DapperContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public System.Data.IDbConnection CreateConnection()
+        {
+            return new System.Data.SqlClient.SqlConnection(_connectionString);
+        }
+    }
+}
